fc8eb843727a231559195dab04ac121c53d52fb0
Genki Takahashi <genki.takahashi.contact@gmail.com>
2018-12-19 14:22:38 +0900

    initial commits for factorization of fish_ditection.ipynb into three files.

---
+++ deepseadream/config.py
@@ -0,0 +1,17 @@
+# should be used in modules
+NUM_CLASSES = len(CLASS)
+IMAGE_SIZE = 256
+IMAGE_PIXELS = IMAGE_SIZE*IMAGE_SIZE*3
+learning_rate = 1e-4
+max_steps = 200
+BATCH_SIZE = 100
+
+PATH = "../data/data/train_fish/"
+ALB = "ALB"
+BET = "BET"
+DOL = "DOL"
+LAG = "LAG"
+NoF = "NoF"
+OTHER = "OTHER"
+SHARK = "SHARK"
+YFT = "YFT"
